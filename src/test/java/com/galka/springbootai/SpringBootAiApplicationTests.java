c8da7140735cba13178167a19fc9c11b601214e4
Michał Gałka <michalgalka84@gmail.com>
2024-03-18 16:10:48 +0100

    test init

---
+++ src/test/java/com/galka/springbootai/SpringBootAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galka.springbootai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
