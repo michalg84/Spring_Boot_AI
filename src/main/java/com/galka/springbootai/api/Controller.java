e043460e81168d16bb20d147b25cc010a1da8604
Michał Gałka <michalgalka84@gmail.com>
2024-03-18 16:10:34 +0100

    create create ChatRequest REST Handler

--- src/main/java/com/galka/springbootai/api/Controller.java
+++ src/main/java/com/galka/springbootai/api/Controller.java
@@ -1,26 +1,54 @@
 package com.galka.springbootai.api;
 
+import ch.qos.logback.classic.LoggerContext;
 import com.galka.springbootai.model.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.Collectors;
+
 @RestController
+@RequestMapping(path = "/api/ollama")
 public class Controller {
+    Logger log = LoggerFactory.getLogger(Controller.class);
 
     @Autowired
     private OllamaChatClient ollamaChatClient;
 
-    @PostMapping("/ask")
-    public String callOllamaRestEntity(@RequestBody Model request) {
+    @PostMapping("/asksimplequestion")
+    public String askSimpleQuestion(@RequestBody Model request) {
+        log.info(request.toString());
         if (!StringUtils.hasText(request.getQuestion())) {
             return "Please provide valid question";
         }
         String response = ollamaChatClient.call(request.getQuestion());
+        log.info(response);
         return response;
+    }
+
+    @PostMapping("/askquestion")
+    public ChatResponse askQuestion(@RequestBody OllamaApi.ChatRequest request) {
+        log.info(request.toString());
+        String questions = request.messages().stream()
+                .map(OllamaApi.Message::content)
+                .collect(Collectors.joining());
+        OllamaOptions ollamaOptions = OllamaOptions.create().withModel(request.model());
+        Prompt prompt = new Prompt(questions, ollamaOptions);
+        ChatResponse chatResponse = ollamaChatClient.call(prompt);
+        log.info(chatResponse.toString());
+        return chatResponse;
     }
 
 }
